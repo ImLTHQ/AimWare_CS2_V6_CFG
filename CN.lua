02f3d58e7f26e469c2c86c1945cd7194207a3acb
1 <1@1.com>
2025-11-17 19:31:01 +0800

    1

--- CN.lua
+++ CN.lua
@@ -1,6 +1,7 @@
 local RF=gui.Reference
 
-local Weapon={"通用","电击枪","手枪","沙鹰与R8","冲锋枪","步枪","散弹枪","鸟狙","连狙","大狙","机枪"}
+local WeaponList={"通用","电击枪","手枪","沙鹰与R8","冲锋枪","步枪","散弹枪","鸟狙","连狙","大狙","机枪"}
 
-local function NM(RF1,NM)  RF1:SetName(NM)  end
+local function NM(RFName,SetName)  RFName:SetName(SetName)  end
+
 NM(RF("Legitbot"),"合法")
